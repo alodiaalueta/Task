8265abdca2c8ef9e5d750e027631359e15ba9bc5
Alodia C. Alueta <alodia.c.alueta@gmail.com>
2025-06-05 13:06:30 +0000

    updated

---
+++ Task10.java
@@ -0,0 +1,27 @@
+// TASK 10 ACTIVITY (Write the method printFullName of student which prints the full name of a student.)
+
+class Student {
+    private String firstName;
+    private String lastName;
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void printFullName() {
+        System.out.println(firstName + " " + lastName);
+    }
+}
+
+public class Task10 {
+    public static void main(String[] args) {
+        Student[] students = new Student[] {
+            new Student("Morgan", "Freeman"),
+            new Student("Brad", "Pitt"),
+            new Student("Kevin", "Spacey"),
+        };
+        for (Student s : students) {
+            s.printFullName();
+        }
+    }
+}
--- Task9.java
+++ Task9.java
@@ -1,40 +1,43 @@
 // TASK 9 ACTIVITY (MATH OPERATIONS WITH STATIC IMPORT)
 
-import static java.lang.Math.*;     
+//import static java.lang.Math.*;     
 public class Task9 {
     
     public static int add(int a, int b) {
-        return addExact(a, b);
+        return Math.addExact(a, b);
     }
 
     
     public static int subtract(int a, int b) {
-        return subtractExact(a, b);
+        return Math.subtractExact(a, b);
     }
 
     
     public static int multiply(int a, int b) {
-        return multiplyExact(a, b);
+        return Math.multiplyExact(a, b);
     }
 
    
     public static int divide(int a, int b) {
-        return floorDiv(a, b); 
+        return Math.floorDiv(a, b); 
     }
 
     
     public static void main(String[] args) {
         int num1 = 20;
         int num2 = 4;
-
+        System.out.println("\n====================");
+        System.out.println("NUMBERS: " + num1 + " & " + num2);
+        System.out.println("====================");
         System.out.println("Addition: " + add(num1, num2));
         System.out.println("Subtraction: " + subtract(num1, num2));
         System.out.println("Multiplication: " + multiply(num1, num2));
         System.out.println("Division: " + divide(num1, num2));
-
-        
-        int x = 15, y = 7;
-        System.out.println("\nUsing values " + x + " and " + y);
+                
+        int x = 7, y = 3;
+        System.out.println("\n====================");
+        System.out.println("NUMBERS: " + x + " & " + y);
+        System.out.println("====================");
         System.out.println("Addition: " + add(x, y));
         System.out.println("Subtraction: " + subtract(x, y));
         System.out.println("Multiplication: " + multiply(x, y));

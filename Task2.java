7038b2d92f6a9e5f20a2655bf575b78202407293
Alodia C. Alueta <alodia.c.alueta@gmail.com>
2025-06-08 12:04:28 +0800

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -2,13 +2,13 @@
 
 class Task2{
     public static void main(String[] args) {
-        byte b1 = 3;
+        int b1 = 3;
         short s1 = 1;
-        int i2 = 0;
+        byte zero = 0;
         char c2 = ' ';
         float f1 = 2.0f;
         boolean bool = true;
 
-        System.out.println("H" + b1 + s1 + s1 + i2 + c2 + "w" + i2 + "r" +s1 +"d " + f1 + c2 + bool);
+        System.out.println("H" + b1 + s1 + s1 + zero + c2 + "w" + zero + "r" +s1 +"d " + f1 + c2 + bool);
     }
 }
